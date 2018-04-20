bb79692e52d31fd866578d48a6c286c04479587b
dom-sultan <dom.sultan@aurea.com>
2018-04-20 14:05:28 +0200

    Init

---
+++ MyApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Ahlan ya 7amada! Welcome to Jenkins!");
+                
+        }
+    }
+}
