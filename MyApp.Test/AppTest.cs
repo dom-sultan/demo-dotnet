0aa60f40eb5d552fec8512f58e8010fd433782e9
dom-sultan <dom.sultan@aurea.com>
2018-04-21 09:11:43 +0200

    Add NUnit and some tests

---
+++ MyApp.Test/AppTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.test
+{
+    [TestFixture]
+    public class AppTest
+    {
+        List<EmployeeDetails> employees;
+
+        [Test]
+        public void CheckDetails()
+        {
+            Program p = new Program();
+            employees = p.AllUsers();
+            foreach (var employee in employees)
+            {
+                Assert.IsNotNull(employee.id);
+                Assert.IsNotNull(employee.Name);
+                Assert.IsNotNull(employee.salary);
+                Assert.IsNotNull(employee.Geneder);
+            }
+        }
+
+        [Test]
+        public void TestLogin()
+        {
+            Program p = new Program();
+            string log1 = p.Login("", "");
+            string log2 = p.Login("wrong", "noway");
+            string log3 = p.Login("Admin", "Admin");
+            Assert.AreEqual("Userid or password could not be Empty.", log1);
+            Assert.AreEqual("Incorrect UserId or Password.", log2);
+            Assert.AreEqual("Welcome Admin.", log3);
+        }
+    }
+}
--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -6,14 +6,90 @@ using System.Threading.Tasks;
 
 namespace MyApp
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) { }
+
+        public string Login(string UserId, string Password)
         {
-            Console.WriteLine("Enter your name:");
-            string name = Console.ReadLine();
-            Console.WriteLine("Ahlan ya 7amada! Welcome to Jenkins!");
-                
+            if (string.IsNullOrEmpty(UserId) || string.IsNullOrEmpty(Password))
+            {
+                return "Userid or password could not be Empty.";
+            }
+            else
+            {
+                if (UserId == "Admin" && Password == "Admin")
+                {
+                    return "Welcome Admin.";
+                }
+                return "Incorrect UserId or Password.";
+            }
         }
+        public List<EmployeeDetails> AllUsers()
+        {
+            List<EmployeeDetails> li = new List<EmployeeDetails>();
+            li.Add(new EmployeeDetails
+            {
+                id = 100,
+                Name = "Bharat",
+                Geneder = "male",
+                salary = 40000
+            });
+            li.Add(new EmployeeDetails
+            {
+                id = 101,
+                Name = "sunita",
+                Geneder = "Female",
+                salary = 50000
+            });
+            li.Add(new EmployeeDetails
+            {
+                id = 103,
+                Name = "Karan",
+                Geneder = "male",
+                salary = 40000
+            });
+            li.Add(new EmployeeDetails
+            {
+                id = 104,
+                Name = "Jeetu",
+                Geneder = "male",
+                salary = 23000
+            });
+            li.Add(new EmployeeDetails
+            {
+                id = 105,
+                Name = "Manasi",
+                Geneder = "Female",
+                salary = 80000
+            });
+            li.Add(new EmployeeDetails
+            {
+                id = 106,
+                Name = "Ranjit",
+                Geneder = "male",
+                salary = 670000
+            });
+            return li;
+        }
+        
+
+        public List<EmployeeDetails> getDetails(int id)
+        {
+            List<EmployeeDetails> li1 = new List<EmployeeDetails>();
+            // Program p = new Program();
+            var li = AllUsers();
+
+            foreach (var x in li)
+            {
+                if (x.id == id)
+                {
+                    li1.Add(x);
+                }
+            }
+            return li1;
+        }
+
+        
     }
 }
